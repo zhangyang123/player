691df2ae27d20721e3c5b0a57020b3a24f234248
zyy123456 <28855932757@qq.com>
2020-04-22 00:48:23 +0800

    更改目录

--- config/index.js
+++ config/index.js
@@ -44,6 +44,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
+    host: 'https://zhangyang123.github.io',
 
     /**
      * Source Maps
